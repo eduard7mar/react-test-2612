472ca19d7a15a8a7642a8af0f38c48ae28c6e709
eduard7mar <edwardmargaryan@gmail.com>
2023-12-26 20:52:36 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function Header() {
+  return(
+    <header>
+      <h3>Header</h3>
+      <span>Timer</span>
+    </header>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello React</h1>
+    </div>
+  )
+}
+
+export default App
