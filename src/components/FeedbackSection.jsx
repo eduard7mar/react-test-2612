141ac45c6915ad098d61c2ecf5f5e102710c0b44
eduard7mar <edwardmargaryan@gmail.com>
2024-01-02 20:52:38 +0200

    added styled-components

--- src/components/FeedbackSection.jsx
+++ src/components/FeedbackSection.jsx
@@ -2,6 +2,10 @@ export default function FeedbackSection() {
   return (
     <section>
       <h3>Feedback</h3>
+
+      <form action="">
+        <input type="text" />
+      </form>
     </section>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,16 @@
 import logo from "/logo-name.svg";
 import { useState } from "react";
+import { styled } from "styled-components";
+
+const HeaderContainer = styled.header`
+  height: 50px;
+  display: flex;
+  padding: 0 2rem;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ccc;
+  background: #fafafa;
+`
 
 export default function Header() {
     const [now, setNow] = useState(new Date());
@@ -7,10 +18,10 @@ export default function Header() {
     setInterval(() => setNow(new Date()), 1000);
 
     return(
-      <header>
+      <HeaderContainer>
         <img src={logo} alt={"Result"} />
         <span>Time: {now.toLocaleTimeString()}</span>
-      </header>
+      </HeaderContainer>
     )
   }
   
